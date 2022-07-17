0fcbb003a965dd3783736996ab42750fab531b17
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-07-17 15:32:38 +0200

    Add 2015/04

---
+++ src/year2015/day04.rs
@@ -0,0 +1,28 @@
+use crate::util::solution;
+
+static INPUT: &str = "ckczppom";
+
+fn find_digest_starting_with(prefix: &str) -> i32 {
+    let mut suffix = 1;
+    loop {
+        let dig = md5::compute(format!("{}{}", INPUT, suffix));
+        let format_dig = format!("{:x}", dig);
+        if format_dig.starts_with(prefix) {
+            return suffix;
+        }
+        suffix += 1;
+    }
+}
+
+fn solution_1() {
+    solution!(find_digest_starting_with("00000"));
+}
+
+fn solution_2() {
+    solution!(find_digest_starting_with("000000"));
+}
+
+pub fn run_day() {
+    solution_1();
+    solution_2();
+}
--- src/year2015/mod.rs
+++ src/year2015/mod.rs
@@ -1,9 +1,11 @@
 pub mod day01;
 pub mod day02;
 pub mod day03;
+pub mod day04;
 
 pub fn run_year() {
     day01::run_day();
     day02::run_day();
     day03::run_day();
+    day04::run_day();
 }
